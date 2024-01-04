1dd662a024596bb77e52e12ca12cfdf026a205de
Krzysztof Ciesielka <szeszej@gmail.com>
2024-01-04 13:49:28 +0100

    event emitting, event receiving

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const path = require("path")
+const http = require("http")
 const express = require("express");
+const socketio = require("socket.io")
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0
+
+io.on("connection", (socket) => {
+  console.log("New client connected")
+  socket.emit("countUpdated", count)
+
+  socket.on("increment", () => {
+    count++
+    // socket.emit("countUpdated", count)
+    io.emit("countUpdated", count)
+  })
+})
+
+server.listen(port, () => {
   console.log("Server is up on port ", port);
 });
